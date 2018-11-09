0aebb4ac25d8fb8e2289193991d86456ee1fcbd2
Wgw <707612123@qq.com>
2018-11-09 15:50:20 +0800

    init

---
+++ src/main/java/ConfigApplication.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p>Title: ConfigApplication</p>
+ * <p>Description: </p>
+ *
+ * @author wgw
+ * @version 1.0.0
+ * @date 2018/11/9 15:22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
